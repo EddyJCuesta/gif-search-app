110bf38f5d71fce8f43f41e6c091e8fc11378c35
EddyJCuesta <cuesta79@gmail.com>
2023-08-03 19:43:10 -0400

    created a gif search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
+
+import GifSearch from './GifSearch';
 import './App.css';
 
 // Create a new React app using the command line
@@ -9,28 +10,11 @@ function App() {
 
 // MY STATES
 const [gifs, setGifs] = useState([])
-// API KEY & URL
-  const apikey = 'PCWbZtP6Jc2hY2TsPujrO9alK4xWo8HW'
-  const url = 'http://api.giphy.com/v1/gifs/search'
+
 
 
 // Create a useEffect for the API Call (API GIPHY Developers) 
 // use Axios to perform the API call
-useEffect(() => {
-  axios({
-    url: url,
-    method: "GET",
-    dataResponse: "json",
-    params: {
-      api_key: apikey,
-      q: 'word'
-    }
-  }).then((res) => {
-    const data = res.data.data;
- // Store the Data in State   
-    setGifs(data);
-  })
-}, [])
 
 // Create a New Component called GifSearch, that is a functional component that renders an input field for the user to search for Gifs
 // Create a handleSearch function which maps through the API data and fetches selected Gifs based on user input, from the API
@@ -39,6 +23,8 @@ useEffect(() => {
   return (
     <>
      <h1>Gif Search</h1> 
+      <GifSearch setGifs={setGifs} />
+      {/* <GifList gifs={gifs} /> */}
     </>
   )
 }
---
+++ src/GifSearch.jsx
@@ -0,0 +1,48 @@
+// GifSearch.js
+import { useState } from 'react';
+import axios from 'axios';
+
+// API KEY & URL
+const apikey = 'PCWbZtP6Jc2hY2TsPujrO9alK4xWo8HW'
+const url = 'http://api.giphy.com/v1/gifs/search'
+
+const GifSearch = ({setGifs}) => {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleChange = (event) =>{
+        setSearchQuery(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    // make a fetch call to the api
+    axios({
+        url: url,
+        method: "GET",
+        dataResponse: "json",
+        params: {
+          api_key: apikey,
+          q: searchQuery
+        }
+      }).then((res) => {
+        const data = res.data.data;
+     // Store the Data in State   
+        setGifs(data);
+      })
+    // with that call store response in state
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleChange}
+          placeholder="Search GIFs..."
+        />
+        <button type="submit">Search</button>
+      </form>
+    );
+}
+
+export default GifSearch;
