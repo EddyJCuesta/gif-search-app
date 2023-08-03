b69ca2d35e8ed86fd374e131a3bc0da326753041
EddyJCuesta <cuesta79@gmail.com>
2023-08-02 21:30:11 -0400

    saved the api data to a state

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-function App() {
-  const apikey = 'PCWbZtP6Jc2hY2TsPujrO9alK4xWo8HW'
-  const url = 'http://api.giphy.com/v1/gifs/search'
-
 // Create a new React app using the command line
 // install Axios
 // Create a App.js app that will hold the main component
-// Create a useEffect for the API Call (API GIPHY Developers) // use Axios to perform the API call
+function App() {
+
+// MY STATES
+const [gifs, setGifs] = useState([])
+// API KEY & URL
+  const apikey = 'PCWbZtP6Jc2hY2TsPujrO9alK4xWo8HW'
+  const url = 'http://api.giphy.com/v1/gifs/search'
+
+
+// Create a useEffect for the API Call (API GIPHY Developers) 
+// use Axios to perform the API call
 useEffect(() => {
   axios({
     url: url,
@@ -18,13 +24,14 @@ useEffect(() => {
     params: {
       api_key: apikey,
       q: 'word'
-
     }
   }).then((res) => {
-    console.log(res)
+    const data = res.data.data;
+ // Store the Data in State   
+    setGifs(data);
   })
-})
-// Store the Data in State
+}, [])
+
 // Create a New Component called GifSearch, that is a functional component that renders an input field for the user to search for Gifs
 // Create a handleSearch function which maps through the API data and fetches selected Gifs based on user input, from the API
 // Create a function that handles the GIFs update returned from the API handleSearch // Create a Gif list Component that Holds the new list of GIFs
