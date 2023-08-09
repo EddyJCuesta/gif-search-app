47f56f14572b768d97147c855a88252aea5fff90
EddyJCuesta <cuesta79@gmail.com>
2023-08-09 18:50:48 -0400

    added a favorite heart to each image

--- src/GifList.jsx
+++ src/GifList.jsx
@@ -1,13 +1,31 @@
 // GifList Component.js
+import { useState } from 'react';
 
 const GifList = ({ gifs }) => {
-    return (
-      <div className="gif-list">
-        {gifs.map((gif) => (
-          <img key={gif.id} src={gif.images.fixed_height.url} alt={gif.title} />
-        ))}
-      </div>
-    );
+  const [favorites, setFavorites] = useState({});
+
+  const toggleFavorite = (gifId) => {
+    setFavorites((preFavorites) => ({
+      ...preFavorites,
+      [gifId]: !preFavorites[gifId],
+    }));
   };
+
+  return (
+    <div className="gif-list">
+      {gifs.map((gif) => (
+        <div key={gif.id} className="gif-container">
+          <img src={gif.images.fixed_height.url} alt={gif.title} />
+          <div
+            className={`heart ${favorites[gif.id] ? 'solid' : 'faded'}`}
+            onClick={() => toggleFavorite(gif.id)}
+          >
+            ❤
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+ };
 
   export default GifList;
